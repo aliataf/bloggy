03a5f2334a8aabe0f3428e13d89997661236758f
Ali Ataf <aliattaf98@gmail.com>
2022-02-27 07:58:41 +0400

    fix: add publicPath to vue config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 module.exports = defineConfig({
 	transpileDependencies: true,
-	outputDir: path.resolve(__dirname, './docs'),
-	assetsDir: 'assets',
+	outputDir: path.resolve(__dirname, 'docs'),
+	publicPath: process.env.NODE_ENV === 'production' ? '/bloggy/' : '/',
 });
